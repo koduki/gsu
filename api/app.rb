65fdf567cc927a2dab06c36b6ab7820fd350859e
koduki <pascalm3+git@gmail.com>
2019-12-21 01:39:32 -0800

    Add web api

---
+++ api/app.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'json'
+require './lib/gadmin_api.rb'
+
+post '/attach/:user_name/:group_name' do |u, g|
+    content_type :json
+
+    admin = GCPAdminAPI.new
+    r = admin.attach_group("#{u}@#{ENV['GCP_DOMAIN']}", "#{g}@#{ENV['GCP_DOMAIN']}")
+    r.to_json
+end
+
+post '/detach/:user_name/:group_name' do |u, g|
+    content_type :json
+
+    admin = GCPAdminAPI.new
+    r = admin.detach_group("#{u}@#{ENV['GCP_DOMAIN']}", "#{g}@#{ENV['GCP_DOMAIN']}")
+    r.to_json
+end
+
+
+get '/groups' do 
+    content_type :json
+
+    admin = GCPAdminAPI.new
+    r = admin.list_groups_all
+    r.to_json
+end
+
+get '/groups/:user_name' do |u|
+    content_type :json
+
+    admin = GCPAdminAPI.new
+    r = admin.list_groups("#{u}@#{ENV['GCP_DOMAIN']}")
+    r.to_json
+end
